7e488e11c705ab4e4094e63edc3b2973a4c89ce5
tyler <tylercobb92@gmail.com>
2022-04-20 20:26:17 -0400

    function to convert currencies from html form

---
+++ app.py
@@ -0,0 +1,19 @@
+from locale import currency
+from flask import Flask, render_template, request, json
+from forex_python.converter import CurrencyRates
+
+app=Flask(__name__)
+app.config['SECRET KEY']="asdfQWERDsfawet"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/convert')
+def convert_currency():
+    c=CurrencyRates()
+    currency_from=c.get_rates(request.args['currencyFrom'])
+    currency_to=c.get_rates(request.args['currencyTo'])
+    start_amount=request.args['amount']
+    finish_amount = c.convert(currency_from, currency_to, start_amount)
+    return render_template('conversion.html', amount=finish_amount)
