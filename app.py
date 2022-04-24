32d644c6cbf346884bfe2bc310e87bd8f4f2f5ed
tyler <tylercobb92@gmail.com>
2022-04-24 17:14:25 -0400

    beginning of error handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from ast import Num
+from flask import Flask, redirect, render_template, request
 from forex_python.converter import CurrencyRates, CurrencyCodes
 
 app=Flask(__name__)
@@ -6,20 +7,40 @@ app.config['SECRET KEY']="asdfQWERDsfawet"
 
 allowed_currencies=['EUR','IDR','BGN','ILS','GBP','DKK','CAD','JPY','HUF','RON','MYR','SEK','SGD','HKD','AUD','CHF','KRW','CNY','TRY','HRK','NZD','THB','USD','NOK','RUB','INR','MXN','CZK','BRL','PLN','PHP','ZAR']
 
+c=CurrencyRates()
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/convert')
+@app.route('/conversion')
 def convert_currency():
-    c=CurrencyRates()
     currency_from=request.args['currencyFrom'].upper()
     currency_to=request.args['currencyTo'].upper()
     start_amount=float(request.args['amount'])
 
-    finish_amount = c.convert(currency_from, currency_to, start_amount)
-    curr_code=CurrencyCodes()
-
-    code=curr_code.get_symbol(currency_to)
+    err_0='All fields required'
+    rule_1=currency_from in allowed_currencies
+    err_1=f'Not a valid code: {currency_from}'
+    rule_2=currency_to in allowed_currencies
+    err_2=f'Not a valid code: {currency_to}'
+    rule_3_1=start_amount>0
+    rule_3_2=start_amount == int
+    rule_3_3=start_amount == float
+    err_3=f'Not a valid amount {start_amount}'
     
-    return render_template('conversion.html', code=code, amount=finish_amount)
+
+    if not currency_from or not currency_to or not start_amount:
+        return render_template('index.html', err_msg=err_0)
+    elif not rule_1:
+        return render_template('index.html', err_msg=err_1)
+    elif not rule_2:
+        return render_template('index.html', err_msg=err_2)
+        
+    else:
+        finish_amount = c.convert(currency_from, currency_to, start_amount)
+        curr_code=CurrencyCodes()
+
+        code=curr_code.get_symbol(currency_to)
+    
+        return render_template('conversion.html', code=code, amount=finish_amount)
