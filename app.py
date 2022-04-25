ff8a1aece15ddf60bd0bc9ed0752a14c443dc7ac
tyler <tylercobb92@gmail.com>
2022-04-24 21:56:06 -0400

    rewrite /conversion route w/ methods=POST

--- app.py
+++ app.py
@@ -13,34 +13,16 @@ c=CurrencyRates()
 def home():
     return render_template('index.html')
 
-@app.route('/conversion')
+@app.route('/conversion', methods=['POST'])
 def convert_currency():
-    currency_from=request.args['currencyFrom'].upper()
-    currency_to=request.args['currencyTo'].upper()
-    start_amount=float(request.args['amount'])
+    currency_from=request.form['currencyFrom'].upper()
+    currency_to=request.form['currencyTo'].upper()
+    start_amount=float(request.form['amount'])
 
-    err_0='All fields required'
-    rule_1=currency_from in allowed_currencies
-    err_1=f'Not a valid code: {currency_from}'
-    rule_2=currency_to in allowed_currencies
-    err_2=f'Not a valid code: {currency_to}'
-    rule_3_1=start_amount>0
-    rule_3_2=start_amount == int
-    rule_3_3=start_amount == float
-    err_3=f'Not a valid amount {start_amount}'
+    finish_amount = c.convert(currency_from, currency_to, start_amount)
+    curr_code=CurrencyCodes()
+
+    code=curr_code.get_symbol(currency_to)
     
-
-    if not currency_from or not currency_to or not start_amount:
-        return render_template('index.html', err_msg=err_0)
-    elif not rule_1:
-        return render_template('index.html', err_msg=err_1)
-    elif not rule_2:
-        return render_template('index.html', err_msg=err_2)
-        
-    else:
-        finish_amount = c.convert(currency_from, currency_to, start_amount)
-        curr_code=CurrencyCodes()
-
-        code=curr_code.get_symbol(currency_to)
-    
-        return render_template('conversion.html', code=code, amount=finish_amount)
+    print(currency_from, currency_to, start_amount)
+    return render_template('conversion.html', code=code, amount=finish_amount)
